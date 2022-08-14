037a1d21e12b46c7d31c89ae271514e83318f4d3
Th3path0 <yaelhichez@gmail.com>
2022-08-13 23:10:30 -0400

    Add files via upload

---
+++ Practica2/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿
+namespace Practica2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Syncfusion.Windows.Forms.Chart.ChartCustomPoint chartCustomPoint1 = new Syncfusion.Windows.Forms.Chart.ChartCustomPoint();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.DGV = new System.Windows.Forms.DataGridView();
+            this.NombreP = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CPU = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Ram = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.PCount = new System.Windows.Forms.ToolStripStatusLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chartControl1 = new Syncfusion.Windows.Forms.Chart.ChartControl();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.DGV, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.listView1, 0, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(57, 28);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 73.70242F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 26.29758F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(862, 578);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // DGV
+            // 
+            this.DGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGV.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.NombreP,
+            this.PID,
+            this.CPU,
+            this.Ram});
+            this.DGV.Location = new System.Drawing.Point(3, 3);
+            this.DGV.Name = "DGV";
+            this.DGV.RowHeadersWidth = 62;
+            this.DGV.RowTemplate.Height = 33;
+            this.DGV.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.DGV.Size = new System.Drawing.Size(710, 408);
+            this.DGV.TabIndex = 0;
+            this.DGV.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DGV_CellContentClick);
+            this.DGV.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DGV_CellDoubleClick);
+            this.DGV.SelectionChanged += new System.EventHandler(this.DGV_SelectionChanged);
+            // 
+            // NombreP
+            // 
+            this.NombreP.HeaderText = "Nombre Proceso";
+            this.NombreP.MinimumWidth = 55;
+            this.NombreP.Name = "NombreP";
+            this.NombreP.ReadOnly = true;
+            this.NombreP.Width = 350;
+            // 
+            // PID
+            // 
+            this.PID.HeaderText = "PID";
+            this.PID.MinimumWidth = 8;
+            this.PID.Name = "PID";
+            this.PID.ReadOnly = true;
+            this.PID.Width = 150;
+            // 
+            // CPU
+            // 
+            this.CPU.HeaderText = "CPU";
+            this.CPU.MinimumWidth = 8;
+            this.CPU.Name = "CPU";
+            this.CPU.ReadOnly = true;
+            this.CPU.Width = 150;
+            // 
+            // Ram
+            // 
+            this.Ram.HeaderText = "Ram";
+            this.Ram.MinimumWidth = 8;
+            this.Ram.Name = "Ram";
+            this.Ram.ReadOnly = true;
+            this.Ram.Width = 150;
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(3, 429);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(856, 146);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.PCount});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 609);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1705, 32);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // PCount
+            // 
+            this.PCount.Name = "PCount";
+            this.PCount.Size = new System.Drawing.Size(70, 25);
+            this.PCount.Text = "PCount";
+            this.PCount.Click += new System.EventHandler(this.PCount_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(965, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // chartControl1
+            // 
+            this.chartControl1.ChartArea.CursorLocation = new System.Drawing.Point(0, 0);
+            this.chartControl1.ChartArea.CursorReDraw = false;
+            chartCustomPoint1.Font.Facename = "Segoe UI";
+            this.chartControl1.CustomPoints.Add(chartCustomPoint1);
+            this.chartControl1.IsWindowLess = false;
+            // 
+            // 
+            // 
+            this.chartControl1.Legend.Location = new System.Drawing.Point(467, 87);
+            this.chartControl1.Localize = null;
+            this.chartControl1.Location = new System.Drawing.Point(1030, 12);
+            this.chartControl1.Name = "chartControl1";
+            this.chartControl1.PrimaryXAxis.LogLabelsDisplayMode = Syncfusion.Windows.Forms.Chart.LogLabelsDisplayMode.Default;
+            this.chartControl1.PrimaryXAxis.Margin = true;
+            this.chartControl1.PrimaryYAxis.LogLabelsDisplayMode = Syncfusion.Windows.Forms.Chart.LogLabelsDisplayMode.Default;
+            this.chartControl1.PrimaryYAxis.Margin = true;
+            this.chartControl1.Size = new System.Drawing.Size(600, 450);
+            this.chartControl1.TabIndex = 3;
+            this.chartControl1.Text = "chartControl1";
+            // 
+            // 
+            // 
+            this.chartControl1.Title.Name = "Default";
+            this.chartControl1.Titles.Add(this.chartControl1.Title);
+            this.chartControl1.VisualTheme = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1705, 641);
+            this.Controls.Add(this.chartControl1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.DGV)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataGridView DGV;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel PCount;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label1;
+        private Syncfusion.Windows.Forms.Chart.ChartControl chartControl1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NombreP;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CPU;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Ram;
+    }
+}
+
---
+++ Practica2/Form1.cs
@@ -0,0 +1,204 @@
+﻿using OxyPlot.Series;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace Practica2
+{
+    public partial class Form1 : Form
+    {
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+            //Obtener();
+            timer1.Enabled = true;
+        }
+
+
+
+        public async Task<string> OBCPU(string P)
+        {
+
+            var Proceso = new Process[0];
+           
+                Proceso = Process.GetProcessesByName(DGV.SelectedRows[0].Cells[0].Value.ToString());
+            
+            var process = "";
+            Parallel.ForEach(Proceso, item => process = item.ProcessName);
+
+            var cpu = new PerformanceCounter("Process", "% Processor Time", process);
+            cpu.NextValue();
+            await Task.Delay(1000);
+            Double porciento = (double)cpu.NextValue() / 10;
+            string number = porciento.ToString();
+            number = Decimal.Parse(porciento.ToString()).ToString("#.##") + "%";
+            if(porciento > 0 && porciento.ToString().Contains("0"))
+            {
+                number = "0" + number;
+                return number;
+            }
+            else
+            {
+                number = "0%";
+            }
+            return number;
+
+        }
+
+        public async Task<string> OBRAM(string P)
+        {
+            var proceso = new Process[0];
+            proceso = Process.GetProcessesByName(DGV.SelectedRows[0].Cells[0].Value.ToString());
+
+            var process = new long();
+            Parallel.ForEach(proceso, item => process = item.PeakWorkingSet64/104857);
+
+            return process.ToString();
+
+        }
+
+        public async Task<string> OBID(string P)
+        {
+            var proceso = new Process[0];
+            proceso = Process.GetProcessesByName(DGV.SelectedRows[0].Cells[0].Value.ToString());
+
+            var process = new long();
+            Parallel.ForEach(proceso, item => process = item.Id);
+
+            return process.ToString();
+
+        }
+
+
+        private void ProcessList()
+        {
+
+            int id = 1;
+
+            Process[] PP = new Process[0];
+
+            foreach (Process p in Process.GetProcesses())
+            {
+
+
+                decimal number = (decimal)p.WorkingSet64;
+                while (Math.Round(number / 1024) >= 1)
+                {
+                    number = number / 1024;
+                }
+
+                number = number / 1024;
+
+
+
+                label1.Text = (number * 100 / DGV.Rows.Count).ToString("0. ## '%'");
+                String porcentaje;
+                porcentaje = label1.Text;
+
+
+
+                DGV.Rows.Add(/*id + ":" + */p.ProcessName, p.Id, p.SessionId, porcentaje); // nombre del proceso
+                id = id + 1;
+
+                //Process[] remoteByName = Process.GetProcessesByName(DGV.SelectedRows[0].Cells[0].Value.ToString());
+
+                //foreach (var pp in remoteByName)
+                //{
+                //    listView1.Items.Add($"Process found with ID: {pp.Id}");
+                //    Thread.Sleep(3000);
+                //}
+
+
+            }
+
+
+
+            PCount.Text = "Procesos Actuales: " + DGV.Rows.Count.ToString();    //  cant de procesos   
+
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ProcessList();
+        }
+        public async Task Obtener()
+        {
+            Process[] TP = Process.GetProcesses();
+            var conteo = 0;
+            try
+            {
+                foreach (Process procesos in Process.GetProcesses())
+                {
+                    //        {
+                    Parallel.ForEach(TP, procesos =>
+                {
+
+                    DGV.Rows.Add(procesos.ProcessName);
+                    conteo++;
+                });
+                }
+            }
+            catch (Exception ex)
+            {
+                ex.Message.ToString();
+            }
+        }
+
+
+
+        private void DGV_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+            
+                //String Nombre = this.DGV.SelectedRows[0].Cells[0].Value.ToString();
+                //int PID = Convert.ToInt32(this.DGV.SelectedRows[0].Cells[1].Value);
+                
+                //decimal RAM = Convert.ToDecimal(this.DGV.SelectedRows[0].Cells[3].Value);
+
+                
+                //MessageBox.Show(
+                //    listView1.Items.Add($""+ $"Process ID: {Value}")
+                //);
+                //listView1.Clear();
+                //listView1.Items.Add($"Proceso: {Nombre}, \n\nPID:{PID} , \n CPU: {CPU} ,\n Ram:{RAM}");
+           
+
+        }
+
+        private async void DGV_SelectionChanged(object sender, EventArgs e)
+        {
+            var ID = await OBID("Proceso");
+            var CPU = await OBCPU("Proceso");
+            var RAM = await OBRAM("Proceso");
+
+            listView1.Items.Add($"" +$"PID:{ID}"+ $"CPU: {CPU}\r\n"+$"RAM: {RAM}");
+
+        }
+
+        private void DGV_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void PCount_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
